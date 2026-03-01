d3bc83df16ab5ead51af145cd879db2c409e1dd2
Reinier de Blois <rdeblois@biblesocieties.org>
2026-03-01 16:29:26 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ const isProd = import.meta.env.PROD;
 
 export default defineConfig({
   output: "static",
-  site: "https://nida-institute.github.io",
+  site: "https://beta.nida-institute.org",
   base: import.meta.env.PROD ? "/nida-website" : "/",
   vite: { plugins: [tailwindcss()] },
 });
